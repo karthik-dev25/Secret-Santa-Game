6ab6aead36503687c276470a24111c00a0e77836
karthikn25 <karthikn25.dev@gmail.com>
2025-04-16 21:15:16 +0530

    Tailwind is Configured

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-console.log("Plain JavaScript")
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+// creating an root Element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendering the Element to the real DOM
+root.render(<App />);
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="font-bold text-3xl">Header is Rendered</div>;
+};
+
+export default Header;
